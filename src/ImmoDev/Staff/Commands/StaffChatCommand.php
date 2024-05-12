7f4602753ef241f4bff34b462006d4556b956d7d
Unreall <128391210+ImmoDevs@users.noreply.github.com>
2024-05-12 12:15:57 +0700

    Update StaffChatCommand.php

--- src/ImmoDev/Staff/Commands/StaffChatCommand.php
+++ src/ImmoDev/Staff/Commands/StaffChatCommand.php
@@ -1,64 +1,209 @@
-<?php 
+<?php
 
 namespace ImmoDev\Staff\Commands;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerChatEvent;
+
+use pocketmine\item\VanillaItems;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat;
 
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerItemUseEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+use pocketmine\network\mcpe\protocol\types\DeviceOS;
+
 use ImmoDev\Staff\Loader;
+use ImmoDev\Staff\Managers\FreezeManager;
 
-class StaffChatCommand extends Command implements Listener {
-
-    private Loader $plugin;
-    private $staffChat = [];
-
-    public function __construct(Loader $plugin) {
-        parent::__construct("staffchat", "Send private message to a staff", null, ["schat"]);
-        $this->setPermission("staffchat.use");
-        $this->plugin = $plugin;
+class StaffToolsCommand extends Command implements Listener
+{
+    public function __construct(Loader $plugin)
+    {
+        parent::__construct("stafftools", "Gives staff tools", null, [
+            "stools",
+        ]);
+        $this->setPermission("stafftools.use");
     }
 
-	public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-		if ($sender instanceof Player) {
-			if ($sender->hasPermission("staffchat.use")) {
-				if (empty($args)) {
-					if (isset($this->staffChat[$sender->getName()])) {
-						unset($this->staffChat[$sender->getName()]);
-						$sender->sendMessage(TextFormat::GREEN . "StaffChat disabled.");
-					} else {
-						$this->staffChat[$sender->getName()] = true;
-						$sender->sendMessage(TextFormat::GREEN . "StaffChat enabled. All your messages will now be sent to StaffChat.");
-					}
-					return true;
-				}
-				$message = implode(" ", $args);
-				$this->sendStaffMessage($sender, $message);
-			} else {
-				$sender->sendMessage(TextFormat::RED . "You do not have permission to use this command.");
-			}
-		} else {
-			$this->plugin->getLogger()->info("This command can only be used in-game.");
-		}
-		return true;
-    }
-
-    public function onPlayerChat(PlayerChatEvent $event): void {
-        $player = $event->getPlayer();
-        if (isset($this->staffChat[$player->getName()])) {
-            $event->cancel();
-            $this->sendStaffMessage($player, $event->getMessage());
+    public function execute(
+        CommandSender $sender,
+        string $commandLabel,
+        array $args
+    ): bool {
+        if (!$sender instanceof Player) {
+            if (!$sender->hasPermission("stafftools.use")) {
+                $sender->sendMessage(
+                    TextFormat::RED .
+                        "You do not have permission to use this command."
+                );
+            } else {
+                $sender->sendMessage(
+                    TextFormat::RED . "This command can only be used in-game."
+                );
+            }
+            return true;
         }
+        $this->StaffTools($sender);
+        return true;
     }
 
-    private function sendStaffMessage(Player $sender, string $message): void {
-        foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
-            if ($player->hasPermission("staffchat.view") || $player->hasPermission("staffchat.use")) {
-                $player->sendMessage(TextFormat::GOLD . "[StaffChat] " . TextFormat::WHITE . $sender->getName() . ": " . $message);
+    public function StaffTools(Player $player)
+    {
+        $compass = VanillaItems::COMPASS()->setCustomName(
+            TextFormat::RESET . TextFormat::AQUA . "Staff Tools - Player Freeze"
+        );
+        $bone = VanillaItems::BONE()->setCustomName(
+            TextFormat::RESET . TextFormat::AQUA . "Staff Tools - Player Info"
+        );
+        $brick = VanillaItems::BRICK()->setCustomName(
+            TextFormat::RESET . TextFormat::AQUA . "Staff Tools - Player Kick"
+        );
+
+        $player->getInventory()->sddItem($compass);
+        $player->getInventory()->sddItem($bone);
+        $player->getInventory()->sddItem($brick);
+    }
+
+    public function onItemUse(PlayerItemUseEvent $event)
+    {
+    }
+
+    public function onEntityDamage(EntityDamageByEntityEvent $event)
+    {
+        $entity = $event->getEntity();
+        $damager = $event->getDamager();
+        if ($entity instanceof Player and $damager instanceof Player) {
+            $item = $damager->getInventory()->getItemInHand();
+            if (
+                $item->getCustomName() ===
+                TextFormat::RESET .
+                    TextFormat::AQUA .
+                    "Staff Tools - Player Freeze"
+            ) {
+                if (!FreezeManager::isPlayerFrozen($entity)) {
+                    FreezeManager::freezePlayer($entity);
+                    $damager->sendMessage(
+                        TextFormat::GOLD .
+                            "Freezing Player: " .
+                            TextFormat::WHITE .
+                            $entity->getName()
+                    );
+                    $damager->sendMessage(
+                        TextFormat::GREEN .
+                            "Player " .
+                            TextFormat::WHITE .
+                            $entity->getName() .
+                            TextFormat::GREEN .
+                            " has been frozen."
+                    );
+                } else {
+                    FreezeManager::unfreezePlayer($entity);
+                    $damager->sendMessage(
+                        TextFormat::GOLD .
+                            "UnFreezing Player: " .
+                            TextFormat::WHITE .
+                            $entity->getName()
+                    );
+                    $damager->sendMessage(
+                        TextFormat::GREEN .
+                            "Player " .
+                            TextFormat::WHITE .
+                            $entity->getName() .
+                            TextFormat::GREEN .
+                            " has been unfrozen."
+                    );
+                }
+                $event->cancel();
+            }
+
+            if (
+                $item->getCustomName() ===
+                TextFormat::RESET .
+                    TextFormat::AQUA .
+                    "Staff Tools - Player Info"
+            ) {
+                $name = $entity->getName();
+                $ping = $entity->getNetWorkSession()->getPing();
+                $health = $entity->getHealth();
+                $ipAddress = $entity->getNetWorkSession()->getIp();
+                $device = $this->getPlayerPlatForm($entity);
+
+                $damager->sendMessage(
+                    TextFormat::GOLD .
+                        "Player Info:" .
+                        "\n" .
+                        TextFormat::GREEN .
+                        "Name: " .
+                        TextFormat::WHITE .
+                        $name .
+                        "\n" .
+                        TextFormat::GREEN .
+                        "Ping: " .
+                        TextFormat::WHITE .
+                        $ping .
+                        "\n" .
+                        TextFormat::GREEN .
+                        "Health: " .
+                        TextFormat::WHITE .
+                        $health .
+                        "\n" .
+                        TextFormat::GREEN .
+                        "IP Address: " .
+                        TextFormat::WHITE .
+                        $ipAddress .
+                        "\n" .
+                        TextFormat::GREEN .
+                        "Device: " .
+                        TextFormat::WHITE .
+                        $device
+                );
+                $event->cancel();
+            }
+
+            if (
+                $item->getCustomName() ===
+                TextFormat::RESET .
+                    TextFormat::AQUA .
+                    "Staff Tools - Player Kick"
+            ) {
+                $name = $entity->getName();
+                $damager->sendMessage(
+                    TextFormat::GOLD .
+                        "Kicking Player: " .
+                        TextFormat::WHITE .
+                        $name
+                );
+                $entity->kick(
+                    TextFormat::RED . "You have been kicked by a staff member."
+                );
+                $event->cancel();
             }
         }
+    }
+
+    public function getPlayerPlatForm(Player $player): string
+    {
+        $data = $player->getPlayerInfo()->getExtraData();
+        if (
+            $data["DeviceOS"] === DeviceOS::ANDROID &&
+            $data["DeviceOS"] === "Not Registered"
+        ) {
+            return "Linux";
+        }
+
+        return match ($data["DeviceOS"]) {
+            DeviceOS::ANDROID => "Android",
+            DeviceOS::IOS => "iOS",
+            DeviceOS::OSX => "Mac",
+            DeviceOS::XBOX => "Xbox",
+            DeviceOS::PLAYSTATION => "PlayStation",
+            DeviceOS::WINDOWS_10 => "Windows 10",
+            DeviceOS::WIN32 => "Windows 32",
+            DeviceOS::NINTENDO => "Nintendo",
+            default => "Unknown",
+        };
     }
 }
